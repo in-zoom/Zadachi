fc4091b5a3734feeffab07ffac54b2636f89c74a
in-zoom <in.zoom@mail.ru>
2019-10-17 18:16:06 +0300

    так правильно?

--- Task_3.go
+++ Task_3.go
@@ -5,12 +5,8 @@ import "fmt"
 //Напишите функцию, которая принимает число, делит его пополам и возвращает true в случае, если исходное число чётное, и false, если нечетное. Например, half(1) должна вернуть (0, false), в то время как half(2) вернет (1, true).
 
 func chislo(xs int) (bool, int) {
-	//y := 2
 	znach := xs / 2
-	//if znach%2 == 0 {
 	return znach%2 == 0, znach
-	//}
-	//return znach%2 == 1
 }
 func main() {
 	xs := 10
